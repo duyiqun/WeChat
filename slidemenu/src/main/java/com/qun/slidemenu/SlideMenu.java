2d678e5070d5b88e17a00a89b63a82cc5ebf5e07
Qun <slvt@qq.com>
2017-05-10 21:55:47 +0800

    限制子View的范围，传递view的偏移量

--- slidemenu/src/main/java/com/qun/slidemenu/SlideMenu.java
+++ slidemenu/src/main/java/com/qun/slidemenu/SlideMenu.java
@@ -19,7 +19,15 @@ import android.widget.FrameLayout;
 public class SlideMenu extends FrameLayout {
 
     private static final String TAG = "SlideMenu";
+    public static final float MULTIPLE = 0.65f;
     private ViewDragHelper mViewDragHelper;
+    private View mMenuView;
+    private View mMainView;
+    private int mMainWidth;
+    private int mMaxRange;
+    private int mMenuWidth;
+    private int mMenuHeight;
+    private int mMainHeight;
 
     public SlideMenu(@NonNull Context context) {
         this(context, null);
@@ -50,12 +58,24 @@ public class SlideMenu extends FrameLayout {
             @Override
             public boolean tryCaptureView(View child, int pointerId) {
                 Log.d(TAG, "tryCaptureView: child=" + child + "/pointerId=" + pointerId);
-                return true;
+                return pointerId == 0;
             }
 
             @Override
             public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
                 super.onViewPositionChanged(changedView, left, top, dx, dy);
+                if (changedView == mMenuView) {
+                    //再将menuView的位置放到原来的位置
+                    mMenuView.layout(0, 0, mMenuWidth, mMenuHeight);
+                    //获取MainView当前的左侧距离，然后累计上menuView的dx偏移量
+                    int newLeft = mMainView.getLeft() + dx;
+                    if (newLeft < 0) {
+                        newLeft = 0;
+                    } else if (newLeft > mMaxRange) {
+                        newLeft = mMaxRange;
+                    }
+                    mMainView.layout(newLeft, 0, newLeft + mMainWidth, mMainHeight);
+                }
             }
 
             @Override
@@ -78,6 +98,13 @@ public class SlideMenu extends FrameLayout {
             @Override
             public int clampViewPositionHorizontal(View child, int left, int dx) {
                 Log.d(TAG, "clampViewPositionHorizontal: child=" + child + "/left=" + left + "/dx=" + dx);
+                if (child == mMainView) {//如果是主布局
+                    if (left < 0) {
+                        left = 0;
+                    } else if (left > mMaxRange) {
+                        left = mMaxRange;
+                    }
+                }
                 return left;
             }
 
@@ -91,7 +118,7 @@ public class SlideMenu extends FrameLayout {
              */
             @Override
             public int clampViewPositionVertical(View child, int top, int dy) {
-                return top;
+                return 0;
             }
         });
     }
@@ -101,5 +128,26 @@ public class SlideMenu extends FrameLayout {
         //还是将触摸事件交给工具类处理
         mViewDragHelper.processTouchEvent(event);
         return true;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int childCount = getChildCount();
+        if (childCount != 2) {
+            throw new RuntimeException("必须有2个布局！");
+        }
+        mMenuView = getChildAt(0);
+        mMainView = getChildAt(1);
+        mMainWidth = mMainView.getMeasuredWidth();
+        mMaxRange = (int) (mMainWidth * MULTIPLE);
+        mMenuHeight = mMenuView.getMeasuredHeight();
+        mMenuWidth = mMenuView.getMeasuredWidth();
+        mMainHeight = mMainView.getMeasuredHeight();
     }
 }
