9a49431049b7017f840532054c6b2771b5c29c88
Qun <slvt@qq.com>
2017-05-10 23:26:46 +0800

    更新包名

--- app/src/main/java/com/qun/weichat/MainActivity.java
+++ app/src/main/java/com/qun/weichat/MainActivity.java
@@ -1,7 +1,9 @@
-package com.qun.wechat;
+package com.qun.weichat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.qun.wechat.R;
 
 public class MainActivity extends AppCompatActivity {
 
