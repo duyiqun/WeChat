f1f4b27be10d68dc4ee7a06c02a6a58b91b69c70
Qun <slvt@qq.com>
2017-05-10 20:43:22 +0800

    修改ArrayAdapter中文本的属性

--- slidemenu/src/main/java/com/qun/slidemenu/MainActivity.java
+++ slidemenu/src/main/java/com/qun/slidemenu/MainActivity.java
@@ -1,9 +1,16 @@
 package com.qun.slidemenu;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
         MainAdapter mainAdapter = new MainAdapter(names);
         mLvMain.setAdapter(mainAdapter);
 
-        mLvMenu.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Cheeses.sCheeseStrings));
+        mLvMenu.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Cheeses.sCheeseStrings){
+
+            //通过复写getView方法，更改字体颜色
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                if(view instanceof TextView){
+                    TextView textView = (TextView) view;
+                    textView.setTextColor(Color.WHITE);
+                }
+                return view;
+            }
+        });
     }
 }
