7b4f68c8343d8e72dc5f2d4dfe07bd38277406d0
Qun <slvt@qq.com>
2017-05-23 00:19:42 +0800

    用Paint画出侧边栏

--- app/src/main/java/com/qun/weichat/widget/SlideBar.java
+++ app/src/main/java/com/qun/weichat/widget/SlideBar.java
@@ -3,9 +3,12 @@ package com.qun.weichat.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hyphenate.util.DensityUtil;
 
 /**
  * Created by Qun on 2017/5/22.
@@ -13,12 +16,25 @@ import android.view.View;
 
 public class SlideBar extends View {
 
+    private static final String[] SECTIONS = {"搜", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private int mX;
+    private int mAvgY;
+    private Paint mPaint;
+
     public SlideBar(Context context) {
         this(context, null);
     }
 
     public SlideBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.parseColor("#9C9C9C"));
+        mPaint.setTextSize(DensityUtil.sp2px(getContext(), 10));
+        mPaint.setTextAlign(Paint.Align.CENTER);
     }
 
     public SlideBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -32,11 +48,17 @@ public class SlideBar extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+        mX = measuredWidth / 2;
+        mAvgY = measuredHeight / SECTIONS.length;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.RED);
+        for (int i = 0; i < SECTIONS.length; i++) {
+            canvas.drawText(SECTIONS[i], mX, mAvgY * (i + 1), mPaint);
+        }
     }
 }
