b7ba343d3db2ef951efba5e1de91f263e6205adf
Qun <slvt@qq.com>
2017-05-24 16:04:49 +0800

    添加好友页面集成SearchView

--- app/src/main/java/com/qun/weichat/view/activity/AddFriendActivity.java
+++ app/src/main/java/com/qun/weichat/view/activity/AddFriendActivity.java
@@ -2,7 +2,10 @@ package com.qun.weichat.view.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +14,7 @@ import com.qun.weichat.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AddFriendActivity extends BaseActivity {
+public class AddFriendActivity extends BaseActivity implements SearchView.OnQueryTextListener {
 
     @BindView(R.id.tv_title)
     TextView mTvTitle;
@@ -21,6 +24,7 @@ public class AddFriendActivity extends BaseActivity {
     ImageView mIvNodata;
     @BindView(R.id.recyclerView)
     RecyclerView mRecyclerView;
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +35,51 @@ public class AddFriendActivity extends BaseActivity {
         mToolBar.setTitle("");
         setSupportActionBar(mToolBar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_friend_menu, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        mSearchView = (SearchView) item.getActionView();
+        mSearchView.setQueryHint("用户名(支持模糊匹配)");
+        //设置SearchView中文本改变监听器
+        mSearchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 当点击软键盘的Action键的时候被回调
+     *
+     * @param query
+     * @return
+     */
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
---
+++ app/src/main/java/com/qun/weichat/view/activity/AddFriendView.java
@@ -0,0 +1,8 @@
+package com.qun.weichat.view.activity;
+
+/**
+ * Created by Qun on 2017/5/24.
+ */
+
+public interface AddFriendView {
+}
