85eece11b9975eb704570547904549f54c152b26
Qun <slvt@qq.com>
2017-05-28 22:35:04 +0800

    编写加载不同布局逻辑及帧动画

--- app/src/main/java/com/qun/weichat/adapter/ChatAdapter.java
+++ app/src/main/java/com/qun/weichat/adapter/ChatAdapter.java
@@ -1,14 +1,20 @@
 package com.qun.weichat.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chat.EMTextMessageBody;
+import com.hyphenate.util.DateUtils;
 import com.qun.weichat.R;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -92,8 +98,80 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     }
 
     @Override
-    public void onBindViewHolder(ChatViewHolder holder, int position) {
+    public void onBindViewHolder(final ChatViewHolder holder, int position) {
+        EMMessage emMessage = mEMMessageList.get(position);
 
+        //显示时间
+        long msgTime = emMessage.getMsgTime();
+        holder.mTvTime.setText(DateUtils.getTimestampString(new Date(msgTime)));
+        if (position == 0) {
+            holder.mTvTime.setVisibility(View.VISIBLE);
+        } else {
+            EMMessage preMessage = mEMMessageList.get(position - 1);
+            long preTime = preMessage.getMsgTime();
+            if (DateUtils.isCloseEnough(msgTime, preTime)) {
+                holder.mTvTime.setVisibility(View.GONE);
+            } else {
+                holder.mTvTime.setVisibility(View.VISIBLE);
+            }
+        }
+
+        //处理发送和接收的文字
+        if (emMessage.direct() == EMMessage.Direct.RECEIVE && emMessage.getType() == EMMessage.Type.TXT) {
+            //接收的文本
+            EMTextMessageBody messageBody = (EMTextMessageBody) emMessage.getBody();
+            String message = messageBody.getMessage();
+            holder.mTvMsg.setText(message);
+        } else if (emMessage.direct() == EMMessage.Direct.SEND && emMessage.getType() == EMMessage.Type.TXT) {
+            //发送的文本
+            EMTextMessageBody messageBody = (EMTextMessageBody) emMessage.getBody();
+            String message = messageBody.getMessage();
+            holder.mTvMsg.setText(message);
+
+            //监听消息的状态
+            emMessage.setMessageStatusCallback(new CallBack() {
+                @Override
+                public void onMainSuccess() {
+                    holder.mIvState.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onMainError(int code, String msg) {
+                    holder.mIvState.setVisibility(View.VISIBLE);
+                    holder.mIvState.setImageResource(R.mipmap.msg_error);
+                }
+
+                @Override
+                public void onMainProgress(int code, String msg) {
+
+                }
+            });
+            //设置mIvState的状态
+            EMMessage.Status status = emMessage.status();
+            switch (status){
+                case CREATE:
+                case INPROGRESS:
+                    showFrameAnimation(holder.mIvState);
+                    break;
+                case SUCCESS:
+                    holder.mIvState.setVisibility(View.INVISIBLE);
+                    break;
+                case FAIL:
+                    holder.mIvState.setVisibility(View.VISIBLE);
+                    holder.mIvState.setImageResource(R.mipmap.msg_error);
+                    break;
+            }
+        }
+    }
+
+    private void showFrameAnimation(ImageView imageState) {
+        imageState.setVisibility(View.VISIBLE);
+        imageState.setImageResource(R.drawable.send_text_animation);
+        AnimationDrawable  animationDrawable = (AnimationDrawable) imageState.getDrawable();
+        if (animationDrawable.isRunning()){
+            animationDrawable.stop();
+        }
+        animationDrawable.start();
     }
 
     @Override
@@ -103,8 +181,17 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
 
     class ChatViewHolder extends RecyclerView.ViewHolder {
 
+        private final TextView mTvTime;
+        private final TextView mTvMsg;
+        private final ImageView mIvImage;
+        private final ImageView mIvState;
+
         public ChatViewHolder(View itemView) {
             super(itemView);
+            mTvTime = (TextView) itemView.findViewById(R.id.tv_time);
+            mTvMsg = (TextView) itemView.findViewById(R.id.tv_msg);
+            mIvImage = (ImageView) itemView.findViewById(R.id.iv_image);
+            mIvState = (ImageView) itemView.findViewById(R.id.iv_state);
         }
     }
 }
