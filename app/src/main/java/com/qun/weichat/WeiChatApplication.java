64733c249f596ea2cebef52ce91a69ec7ff135ea
y480-pc\Qun <slvt@qq.com>
2017-06-27 18:30:40 +0800

    使用SoundPool给消息加声音，后台接收消息显示通知栏，并添加通知栏功能，设置任务栈

--- app/src/main/java/com/qun/weichat/WeiChatApplication.java
+++ app/src/main/java/com/qun/weichat/WeiChatApplication.java
@@ -1,8 +1,20 @@
 package com.qun.weichat;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Vibrator;
 import android.util.Log;
 
 import com.avos.avoscloud.AVOSCloud;
@@ -11,12 +23,17 @@ import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.chat.EMTextMessageBody;
 import com.hyphenate.exceptions.HyphenateException;
 import com.qun.weichat.db.DBUtils;
 import com.qun.weichat.event.ContactUpdateEvent;
+import com.qun.weichat.view.activity.ChatActivity;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -27,6 +44,12 @@ import java.util.List;
 public class WeiChatApplication extends Application {
 
     private static final String TAG = "WeiChatApplication";
+    private SoundPool mSoundPool;
+    private int mDuanSound;
+    private int mYuluSound;
+    private NotificationManager mNotificationManager;
+    private HashMap<String, Integer> mHashMap = new HashMap();
+    private List<Activity> mBaseActivities = new ArrayList<>();
 
     @Override
     public void onCreate() {
@@ -34,6 +57,20 @@ public class WeiChatApplication extends Application {
         initEaseMob();
         initAVOSCloud();
         initDB();
+        initSoundPool();
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private void initSoundPool() {
+        mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        try {
+            AssetFileDescriptor duanAFD = getAssets().openFd("duan.mp3");
+            AssetFileDescriptor yuluAFD = getAssets().openFd("yulu.mp3");
+            mDuanSound = mSoundPool.load(duanAFD, 1);
+            mYuluSound = mSoundPool.load(yuluAFD, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initDB() {
@@ -141,6 +178,16 @@ public class WeiChatApplication extends Application {
                 //收到消息
                 Log.d(TAG, "onMessageReceived: " + list.get(0).toString());
                 EventBus.getDefault().post(list.get(0));
+
+                boolean isRunBackground = isRunInBackGround();
+                if (isRunBackground) {
+                    mSoundPool.play(mYuluSound, 1, 1, 0, 0, 1);
+                    Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                    vibrator.vibrate(2000);
+                    sendNotification(list.get(0));
+                } else {
+                    mSoundPool.play(mDuanSound, 1, 1, 0, 0, 1);
+                }
             }
 
             @Override
@@ -163,5 +210,66 @@ public class WeiChatApplication extends Application {
                 //消息状态变动
             }
         });
+    }
+
+    private void sendNotification(EMMessage emMessage) {
+        String userName = emMessage.getUserName();
+        int id = 1;
+        if (mHashMap.containsKey(userName)) {
+            id = mHashMap.get(userName);
+        } else {
+            mHashMap.put(userName, mHashMap.size() + 1);
+            id = mHashMap.size();
+        }
+        String msg = "";
+        switch (emMessage.getType()) {
+            case TXT:
+                EMTextMessageBody textMessageBody = (EMTextMessageBody) emMessage.getBody();
+                msg = textMessageBody.getMessage();
+                break;
+            case IMAGE:
+                msg = "【图片】";
+                break;
+            default:
+                msg = "未知类型";
+                break;
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.largmessage);
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent chatIntent = new Intent(this, ChatActivity.class);
+        chatIntent.putExtra("username", userName);
+        Intent[] intents = {mainIntent, chatIntent};
+
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, id, intents, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(getApplicationContext())
+                .setSmallIcon(R.mipmap.message)
+                .setLargeIcon(bitmap)
+                .setContentTitle("你有新消息")
+                .setContentText(msg)
+                .setContentInfo(userName)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setAutoCancel(true)
+                //设置延时意图，就是指定点击通知时的动作
+                .setContentIntent(pendingIntent)
+                .build();
+
+        mNotificationManager.notify(id, notification);
+    }
+
+    private boolean isRunInBackGround() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        /**
+         * 获取手机中当前运行的任务栈
+         */
+        List<ActivityManager.RunningTaskInfo> runningTasks = activityManager.getRunningTasks(100);
+        //获取最前面的任务栈
+        ActivityManager.RunningTaskInfo runningTaskInfo = runningTasks.get(0);
+        //获取这个任务栈中最顶端的Activity
+        ComponentName topActivity = runningTaskInfo.topActivity;
+        //判断这个Activity的包名是否跟我们的包名一致
+        return !topActivity.getPackageName().equals(getPackageName());
     }
 }
